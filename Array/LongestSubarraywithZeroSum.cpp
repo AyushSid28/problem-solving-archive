293f133ec789a701e6ea173b87a9945c617e9cdd
AyushSid28 <ayushsiddhant2@gmail.com>
2026-03-20 00:01:46 +0530

    Added code for LongestSubarray with Zero sum

--- Array/LongestSubarraywithZeroSum.cpp
+++ Array/LongestSubarraywithZeroSum.cpp
@@ -6,7 +6,14 @@ class Solution{
         for (int i=0;i<n;i++){
             int sum=0;
             for(int j=i;j<n;j++){
+                sum=nums[j];
+                if(sum==0){
+                    int ans=j-i+1;
+                    maxi=max(maxi,ans);
 
+                }
             }
         }
+        return maxi;
+}
 }
