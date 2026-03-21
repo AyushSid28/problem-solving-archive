0dee1844045fd2df71db3f3fa6bd7b4b0036538a
AyushSid28 <ayushsiddhant2@gmail.com>
2026-03-21 17:31:22 +0530

    Added code for Upper Bound problem

---
+++ Binary Search/UpperBound.cpp
@@ -0,0 +1,51 @@
+//Problem Statement: Given a sorted array of N integers and an integer x, write a program to find the upper bound of x.
+
+
+
+// Example 1:
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+
+// Example 2:
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+
+
+class Solution{
+    public:
+        int upperBound(vector<int>& nums,int target){
+            int n=nums.size();
+            int left=0;
+            int right=n-1;
+            int ans=n;
+            while(left<=right){
+                int mid=left+right/2;
+                if(nums[mid]>target){
+                    ans=mid;
+                    right=mid-1;
+                }
+                else{
+                    left=mid+1;
+                }
+            }
+            return ans;
+
+        }
+    }
+
+
+//2nd approach
+
+class Solution{
+    public:
+       int upperBound(vector<int>& nums,int target){
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]>target){
+                return i;
+            }
+        }
+        return n;
+}
+
+}
