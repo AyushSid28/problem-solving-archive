1eacb7ea6ddf3bfc58a95ad4b2bdbae5e1ef0127
AyushSid28 <ayushsiddhant2@gmail.com>
2026-04-06 11:58:07 +0530

    Added solution for rotating an array

--- Array/RearrangeEleBySign.cpp
+++ Array/RearrangeEleBySign.cpp
@@ -31,3 +31,25 @@ class Solution{
         return nums;
        }
 }
+
+
+
+//Approach 2
+
+class Solution{
+    public:
+        vector<int> rearrangeEleBySign(vector<int>& nums){
+            int n=nums.size();
+            vector<int> ans(n,0); //n is size of array and 0 denotes all the elements are set to 0
+            int pos=0,neg=1;
+            for(int i=0;i<n;i++){
+                if(nums[i]<0){
+                    ans[neg]=nums[i];
+                }
+                else{
+                    ans[pos]=nums[i];
+                }
+            }
+            return ans;
+}
+}
