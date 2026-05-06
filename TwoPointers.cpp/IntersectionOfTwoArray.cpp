1c964cec84f3555d51a8beb314846b8faa5993e6
AyushSid28 <ayushsiddhant2@gmail.com>
2026-05-06 20:49:08 +0530

    Made some modification for intersection to avoid duplicates

--- TwoPointers.cpp/IntersectionOfTwoArray.cpp
+++ TwoPointers.cpp/IntersectionOfTwoArray.cpp
@@ -18,6 +18,8 @@ class Solution {
         vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
             vector<int>result;
             int i=0,j=0;
+            sort(nums1.begin(),nums1.end());
+            sort(nums2.begin(),nums2.end());
             while(i<nums1.size() && j<nums2.size()){
                 if(nums1[i]<nums2[j]){
                     i++;
@@ -26,7 +28,9 @@ class Solution {
                     j++;
                 }
                 else{
-                    result.push_back(nums[i]);
+                    if(result.empty() || result.back() != nums1[i]) {
+                        result.push_back(nums1[i]);
+                    }
                     i++;
                     j++;
     
