07702b0586bdc19e749c5f7ab97288da7798719d
AyushSid28 <ayushsiddhant2@gmail.com>
2026-05-06 15:04:56 +0530

    Added code for merging to array

---
+++ TwoPointers.cpp/MergeSortedArr.cpp
@@ -0,0 +1,41 @@
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+// Example 1:
+// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// Output: [1,2,2,3,5,6]
+// Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+// The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+
+class Solution {
+    public:
+        void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+            vector<int>result;
+            int i=0,j=0;
+            while(i<m && j<n){
+                if(nums1[i]<=nums2[j]){
+                    result.push_back(nums1[i]);
+                    i++;
+                }
+                else{
+                    result.push_back(nums2[j]);
+                    j++;
+                }
+            }
+            while(i< m){
+                result.push_back(nums1[i]);
+                i++;
+              
+            };
+            while(j<n){
+                result.push_back(nums2[j]);
+                j++;
+              
+            };
+            for(int k=0;k<m+n;k++){
+                nums1[k]=result[k];
+            }
+            
+        }
+    };
