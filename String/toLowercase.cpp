7e488771ae2581c6e70f1a9569eccda139c1ce11
AyushSid28 <ayushsiddhant2@gmail.com>
2026-05-09 16:28:56 +0530

    Added code for converting string to lowercase

---
+++ String/toLowercase.cpp
@@ -0,0 +1,29 @@
+// Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+ 
+
+// Example 1:
+
+// Input: s = "Hello"
+// Output: "hello"
+// Example 2:
+
+// Input: s = "here"
+// Output: "here"
+// Example 3:
+
+// Input: s = "LOVELY"
+// Output: "lovely"
+
+class Solution {
+    public:
+        string toLowerCase(string s) {
+            string result="";
+            for(char ch:s){
+                char j=tolower(ch);
+                result.push_back(j);
+            }
+            
+            return result;
+        }
+    };
