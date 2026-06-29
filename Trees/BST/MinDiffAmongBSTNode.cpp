369984cb7d596a209d35f9626a9da61620e5a677
AyushSid28 <ayushsiddhant2@gmail.com>
2026-06-29 21:41:53 +0530

    Added code for minDiff Among BST Node

---
+++ Trees/BST/MinDiffAmongBSTNode.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    public:
+        int minDiff=INT_MAX;
+        TreeNode* prev=nullptr;
+            
+        int minDiffInBST(TreeNode* root) {
+           
+           inorder(root);
+           return minDiff;
+    
+        }
+        void inorder(TreeNode* root){
+            if(root==nullptr) return;
+            inorder(root->left);
+            if(prev!=nullptr) minDiff=min(minDiff,root->val-prev->val);
+            prev=root;
+    
+            inorder(root->right);
+        }
+    };
